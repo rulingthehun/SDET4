7dece2fb0bf59b22f714d5ef17a7c68fb56b2332
rulingthehun <114784395+rulingthehun@users.noreply.github.com>
2023-03-18 23:59:52 +0200

    XPath - select - hiddenElement

--- src/Gun04/_01_OpeningWebsite.java
+++ src/Gun04/_01_OpeningWebsite.java
@@ -2,15 +2,10 @@ package Gun04;
 
 import Utility.MyFunc;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.safari.SafariDriver;
 
 public class _01_OpeningWebsite {
     public static void main(String[] args) {
-        System.setProperty(ChromeDriverService.CHROME_DRIVER_SILENT_OUTPUT_PROPERTY,"true");
-        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
-        //WebDriver driver = new ChromeDriver();
         WebDriver driver = new SafariDriver(); // web sayfasini kontrol eden gorevli
         driver.manage().window().maximize();
 
@@ -19,6 +14,6 @@ public class _01_OpeningWebsite {
         MyFunc.Bekle(3);
         driver.quit();
         int sayi = 5;
-
+        System.out.println("sayi = " + sayi%3);
     }
 }
--- src/Utility/BaseDriverFirefox.java
+++ src/Utility/BaseDriverFirefox.java
@@ -26,8 +26,8 @@ public class BaseDriverFirefox {
         driver.manage().window().maximize(); //Tam ekran
         driver.manage().deleteAllCookies(); //Sitenin bilgisayarda yağtığı ayarları siler
 
-        Duration dr = Duration.ofSeconds(20);
-        driver.manage().timeouts().pageLoadTimeout(dr); //Sadece tüm sayfanın kodlarının
+        Duration dr = Duration.ofSeconds(2);
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(5)); //Sadece tüm sayfanın kodlarının
         // bilgisayara inme süresi ile ilgili
         //Bu eklenmezse Selenium sayfa yüklenene kadar (sonsuza) bekler. : 30 sn süresincesayfanın
         // yüklenmesini bekle, yüklenmezse hata ver.
